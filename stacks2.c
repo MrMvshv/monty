a3bd627569e06a36bdfdc025b1ddab385fb93478
MrMvshv <machariajosphat001@gmail.com>
2022-12-16 00:37:22 +0000

    mo opsi 2

---
+++ stacks2.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+/**
+ * Add - adds top 2
+ * @stack: stack
+ * @line: line
+ *
+ * Return: void
+ */
+void Add(stack_t **stack, unsigned int line)
+{
+	int j = 0;
+	stack_t *a = NULL;
+
+	a = *stack;
+	for (; a; a = a->next)
+		j++;
+	if (j < 2)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
+	a = (*stack)->next;
+	a->n += (*stack)->n;
+	pOp(stack, line);
+}
+
+/**
+ * nOp - nothing
+ * @stack: stack
+ * @line: line
+ *
+ * Return: void
+ */
+void nOp(stack_t **stack, unsigned int line)
+{
+	(void) line;
+	(void) stack;
+}
